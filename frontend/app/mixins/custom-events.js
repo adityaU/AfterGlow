7dd85efc5c34bf06f5808614c27250ad522a83e1
Aditya Upadhyay <adityau@Adityas-MacBook-Pro.local>
2018-04-27 23:22:32 +0530

    fix lengend

--- frontend/app/mixins/custom-events.js
+++ frontend/app/mixins/custom-events.js
@@ -4,4 +4,4 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
     plotlyResize: new Event('plotlyResize')
-})
+});
--- frontend/app/mixins/utils-functions.js
+++ frontend/app/mixins/utils-functions.js
@@ -1,27 +1,27 @@
 import Ember from 'ember';
-import ColorMixin from 'frontend/mixins/colors-mixin'
-import ResultViewMixin from 'frontend/mixins/result-view-mixin'
-import HelperMixin from 'frontend/mixins/helper-mixin'
+import ColorMixin from 'frontend/mixins/colors-mixin';
+import ResultViewMixin from 'frontend/mixins/result-view-mixin';
+import HelperMixin from 'frontend/mixins/helper-mixin';
 
 export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
     display(params){
         var formattedString = params;
-        var objType = (Object.prototype.toString.call(params).replace(/\[object|\]/g, "").trim())
+        var objType = (Object.prototype.toString.call(params).replace(/\[object|\]/g, '').trim());
         if (objType == 'Object'){
             formattedString = JSON.stringify(params);
         }else if (objType == 'Array'){
             formattedString = params.map((item) => {
                 if (typeof(item) == 'object'){
-                    return JSON.stringify(params)
+                    return JSON.stringify(params);
                 }else{
                     return params;
                 }
-            })
+            });
         }
-        let date = Date.parse(params) 
-        let dateMatch = (params && params.toString().match("-") != null)
+        let date = Date.parse(params); 
+        let dateMatch = (params && params.toString().match('-') != null);
         if (date.toString() != 'NaN' && dateMatch){
-                formattedString = moment(date).format("YYYY-MM-DD HH:mm:ss.SSS")
+            formattedString = moment(date).format('YYYY-MM-DD HH:mm:ss.SSS');
         }
 
         return formattedString;
@@ -42,31 +42,31 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                 {
                     return Plotly.toImage(gd,{format:'png',height:1600,width:1600});
                 }
-            )
+            );
     },
     gridParent: Ember.computed(function(){
-        return this.$("#" + this.get('randomId')).parents('.grid-stack-item')
+        return this.$('#' + this.get('randomId')).parents('.grid-stack-item');
     }),
     dimensions(gridParent){
-        let dimensions = {}
+        let dimensions = {};
         if (gridParent && gridParent[0]){
             dimensions = {
                 height: gridParent.innerHeight() - 90,
-                width:  gridParent.innerWidth() - 80
-            }
+                width:  gridParent.innerWidth() - 30
+            };
         }
         return dimensions;
     },
     getNode(_this) {
-        var d3 = Plotly.d3
-        let gridParent = _this.get('gridParent')
+        var d3 = Plotly.d3;
+        let gridParent = _this.get('gridParent');
         if (!_this.get('chosenColor')){
-            _this.set('chosenColor', _this.randomColor())
+            _this.set('chosenColor', _this.randomColor());
         }
-        let dimensions = _this.get('dimensions')(gridParent)
-        dimensions.height && (dimensions.height += "px")
-        dimensions.width && (dimensions.width += "px")
-        var gd3 = d3.select("#" + _this.get('randomId'))
+        let dimensions = _this.get('dimensions')(gridParent);
+        dimensions.height && (dimensions.height += 'px');
+        dimensions.width && (dimensions.width += 'px');
+        var gd3 = d3.select('#' + _this.get('randomId'))
                 .style(dimensions);
 
         return gd3.node();
@@ -100,17 +100,17 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                              'multipleYs.@each.chartType',
                              'multipleYs.@each.lineShape',
                              'results', function(){
-        var data = this.get('results');
-        var multipleYs = this.get('multipleYs');
-        var x1 = this.get('x1');
-        var x2 = this.get('x2');
-        if (data && x1 && multipleYs && multipleYs.length >=0){
+                                 var data = this.get('results');
+                                 var multipleYs = this.get('multipleYs');
+                                 var x1 = this.get('x1');
+                                 var x2 = this.get('x2');
+                                 if (data && x1 && multipleYs && multipleYs.length >=0){
             var x2 = data.columns.indexOf(x2);
             var x1 = data.columns.indexOf(x1);
             var multipleYs = multipleYs.map((y)=> { 
-                return y ? data.columns.indexOf(y.columnName) : -1 }
+                return y ? data.columns.indexOf(y.columnName) : -1; }
             ).filter((i)=>{
-                return i >= 0
+                return i >= 0;
             });
             data = multipleYs.map((y)=>{
                 let d = data.rows.map((item)=>{
@@ -121,18 +121,18 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                         displayX2: this.get('display')(item[x2]),
                         y: item[y],
                         displayY: this.get('display')(item[y]),
-                    })
-                })
-                return this.get('groupBy')(d, 'x2')
+                    });
+                });
+                return this.get('groupBy')(d, 'x2');
             });
             return data;
         }
-    }),
+                             }),
     x1: Ember.computed.alias('resultsViewSettings.x1'),
     resultsObserverThatSetsX1: Ember.on('init', Ember.observer('results', 'x1', function(){
         if (!this.get('resultsViewSettings.x1')){
             let results = this.get('results');
-            let rows = results &&  results.rows.length && results.rows[0]
+            let rows = results &&  results.rows.length && results.rows[0];
             let found = null;
 
             if (this.get('resultsViewType') == 'Pie'){
@@ -142,7 +142,7 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                         if (this.findIfNumber(rows[i])) { (found = i); break;}
                     }
                     if ( found == 0 || found){
-                        this.set('x1', results.columns[i])
+                        this.set('x1', results.columns[i]);
                     }
                 }
             }else{
@@ -151,13 +151,13 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                         if (this.findIfDate(rows[i])) { (found = i); break;}
                     }
                     if ( found == 0 || found){
-                        return this.set('x1', results.columns[i])
+                        return this.set('x1', results.columns[i]);
                     }
                     for (var i=0; i<rows.length; i++){
                         if (this.findIfNumber(rows[i])) { (found = i); break;}
                     }
                     if ( found == 0 || found){
-                        return this.set('x1', results.columns[i])
+                        return this.set('x1', results.columns[i]);
                     }
                 }
             }
@@ -174,22 +174,22 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
     resultsObserverThatSetsX2: Ember.on('init', Ember.observer('results', 'x2', function(){
         if (!this.get('resultsViewSettings.x1')){
             let results = this.get('results');
-            let rows = results &&  results.rows.length && results.rows[0]
-            let row = rows && rows.length && rows[0]
-            let found = null
+            let rows = results &&  results.rows.length && results.rows[0];
+            let row = rows && rows.length && rows[0];
+            let found = null;
             if (!(this.get('resultsViewType') == 'Pie')){
                 if (row){
                     for (var i=0; i<row.length; i++){
                         if (!(this.findIfDate(row[i]) || this.findIfNumber(row[i]))) {
                             let items = rows.map(function(item){
-                                return item && item[i]
-                            })
-                            let canBeSet = (this.unique(items)).length <= 10
+                                return item && item[i];
+                            });
+                            let canBeSet = (this.unique(items)).length <= 10;
                             if (canBeSet){ (found = i); break;}
                         }
                     }
                     if (found == 0 || found){
-                        this.set('x2', results.columns[i])
+                        this.set('x2', results.columns[i]);
                     }
                 }
             }
@@ -198,12 +198,12 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
 
     resultsObserverThatSetsMultipleYs: Ember.on('init', Ember.observer('results', 'multipleYs', function(){
         let results = this.get('results');
-        let rows = results &&  results.rows.length && results.rows[0]
-        let multipleYs = this.get('resultsViewSettings.multipleYs')
-        let found = null
+        let rows = results &&  results.rows.length && results.rows[0];
+        let multipleYs = this.get('resultsViewSettings.multipleYs');
+        let found = null;
         let lengthMultipleYs = multipleYs && multipleYs.filter((item)=>{
-            return item.columnName
-        }).length
+            return item.columnName;
+        }).length;
         if (!multipleYs || lengthMultipleYs == 0){
             if (rows){
                 if (this.get('resultsViewType') == 'Pie'){
@@ -213,23 +213,23 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                         }
                     }
                     if ( found == 0 || found){
-                        this.set('multipleYs', [{columnName: results.columns[i]}])
+                        this.set('multipleYs', [{columnName: results.columns[i]}]);
                     }
                 }else{
-                    let count = 0
+                    let count = 0;
                     let multipleYs = rows.filter((item, i)=>{
                         let canBeSet = count < 4
                             && !this.findIfDate(item)
-                            && this.findIfNumber(item)
-                        canBeSet && (count += 1)
-                        return canBeSet
+                            && this.findIfNumber(item);
+                        canBeSet && (count += 1);
+                        return canBeSet;
                     }).map((item)=>{
-                        return {columnName: results.columns[rows.indexOf(item)]}
-                    })
+                        return {columnName: results.columns[rows.indexOf(item)]};
+                    });
                     if (multipleYs.length == 0){
-                        this.set('multipleYs', [{}])
+                        this.set('multipleYs', [{}]);
                     }else{
-                        this.set('multipleYs', multipleYs)
+                        this.set('multipleYs', multipleYs);
                     }
                 }
             }
@@ -239,16 +239,16 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
     x2: Ember.computed.alias('resultsViewSettings.x2'),
     multipleYs: Ember.computed.alias('resultsViewSettings.multipleYs'),
     ys: Ember.on('init', Ember.observer('resultsViewSettings.y', function(){
-        let multipleYs = this.get('multipleYs')
-        let y = this.get('resultsViewSettings.y') || this.get('y')
+        let multipleYs = this.get('multipleYs');
+        let y = this.get('resultsViewSettings.y') || this.get('y');
         if (y && !multipleYs){
-            this.set('multipleYs', [{columnName: y}])
+            this.set('multipleYs', [{columnName: y}]);
         }else if (y && multipleYs){
             if (!multipleYs.findBy('columnName', y))
-            multipleYs.pushObject({columnName: y})
+                multipleYs.pushObject({columnName: y});
         }else if (!y && multipleYs){
         }else{
-            this.set('multipleYs', [{columnName: y}])
+            this.set('multipleYs', [{columnName: y}]);
         } 
     })),
     y: Ember.computed.alias('resultsViewSettings.y'),
@@ -260,62 +260,62 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
         return  'chart-' + Math.floor((Math.random() * 100000000000000) + 1);
     }),
 
-    layout: Ember.computed('title', 'margin', "xLabel","yLabel", "jsonData", function(){
+    layout: Ember.computed('title', 'margin', 'xLabel','yLabel', 'jsonData', function(){
         let l  = {
-            legend: {orientation: "h", x:0, y:1},
+            legend: { orientation: 'v', x: 100, y: 0 },
             title: this.get('title'),
             margin: this.get('margin'),
             xaxis: {showgrid: false, zeroline: false, title: Ember.String.capitalize(this.get('xLabel') || this.get('x1')) , autorange: true, showLine: false },
-            yaxis: {showgrid: true, zeroline: false, gridcolor: "#f1f1f1", title: Ember.String.capitalize(this.get('yLabel') || this.get('multipleYs')[0].columnName), autorange: true, showLine: false},
+            yaxis: {showgrid: true, zeroline: false, gridcolor: '#f1f1f1', title: Ember.String.capitalize(this.get('yLabel') || this.get('multipleYs')[0].columnName), autorange: true, showLine: false},
             font: {
                 family: 'Lato',
                 size: '1em',
                 color: '#212133'
             }
 
-        }
+        };
 
         this.get('multipleYs').forEach((item, i)=>{
             if ( item && item.separateYaxis && i !=0 ){
-                let yaxisName = "yaxis" + (i + 1).toString()
+                let yaxisName = 'yaxis' + (i + 1).toString();
                 l[yaxisName] = {
                     title:  item && item.columnName,
                     titlefont: {color: this.get('colors')[i]},
                     tickfont: {color: this.get('colors')[i]},
                     overlaying: 'y',
-                    side: "right"
-                }
-                l["margin"] = null
+                    side: 'right'
+                };
+                l['margin'] = null;
             }
-        })
-        return l
+        });
+        return l;
 
     }),
 
     legendName(item, i){
-       return  item.get('type') || this.get('multipleYs')[i].columnName
+        return  item.get('type') || this.get('multipleYs')[i].columnName;
     },
 
     getChartType(i){
-        let chartType = this.get('multipleYs')[i].chartType
-        let defaultChartType = this.get('defaultChartType')
-        return chartType || defaultChartType
+        let chartType = this.get('multipleYs')[i].chartType;
+        let defaultChartType = this.get('defaultChartType');
+        return chartType || defaultChartType;
     },
 
     mode(item){
         if (item.length >= 31){
-            return "lines"
+            return 'lines';
         }else{
-            return "lines+markers"
+            return 'lines+markers';
         }
     },
 
 
     chartData(item, i, j, type, _this){
-        let x =  item.get('contents').sortBy('x1').map((el)=>{ return el.get('displayX1')})
-        let y =   item.get('contents').sortBy('x1').map((el)=>{ return el.get('displayY')})
-        let d = null
-        if (type == "Line"){
+        let x =  item.get('contents').sortBy('x1').map((el)=>{ return el.get('displayX1');});
+        let y =   item.get('contents').sortBy('x1').map((el)=>{ return el.get('displayY');});
+        let d = null;
+        if (type == 'Line'){
             d = {
                 x: x,
                 y: y,
@@ -327,8 +327,8 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                     color: _this.get('colors')[i+j]
                 },
                 name:  _this.legendName(item, i)
-            }
-        }else if(type == "Bars"){
+            };
+        }else if(type == 'Bars'){
             d = {
                 x: x,
                 y: y,
@@ -337,8 +337,8 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                     color: _this.get('colors')[i + j] 
                 },
                 name: _this.legendName(item, i)
-            }
-        }else if (type == "Area"){
+            };
+        }else if (type == 'Area'){
             d = {
                 x: x,
                 y: y,
@@ -349,29 +349,29 @@ export default Ember.Mixin.create(ColorMixin, ResultViewMixin, HelperMixin, {
                     color: _this.get('colors')[i+j]
                 },
                 name: _this.legendName(item, i)
-            }
+            };
 
-        }else if (type == "Bubble"){
-            let total = item.get('contents').sortBy('x1').map((el)=>{ return el.get('y')}).reduce((a,b)=> { return a+b}, 0)
+        }else if (type == 'Bubble'){
+            let total = item.get('contents').sortBy('x1').map((el)=>{ return el.get('y');}).reduce((a,b)=> { return a+b;}, 0);
             d = {
                 x: x,
                 y: y,
                 type: 'scatter',
                 mode: 'markers',
                 marker: {
-                    size:  item.get('contents').sortBy('x1').map((el)=>{ return (+el.get('y')/ total)*600}),
+                    size:  item.get('contents').sortBy('x1').map((el)=>{ return (+el.get('y')/ total)*600;}),
                     color: _this.get('colors')[i + j],
                 },
                 name: _this.legendName(item, i)
-            }
+            };
         }
 
         _this.get('multipleYs').forEach((item, j)=>{
             if ( item && item.separateYaxis && j !=0 && i == j){
-                d["yaxis"] = "y" + (i + 1).toString()
+                d['yaxis'] = 'y' + (i + 1).toString();
             }
-        })
-        return d
+        });
+        return d;
 
     }
     // chartLine(){},
