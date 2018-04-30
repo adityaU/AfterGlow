f278ab5989dde7cc022efb713d70b17060534164
Aditya <im.adityau@gmail.com>
2018-04-30 21:16:30 +0530

    remove IEx.pry from variables

--- web/controllers/variable_controller.ex
+++ web/controllers/variable_controller.ex
@@ -15,9 +15,6 @@ defmodule AfterGlow.VariableController do
 
   def create(conn, %{"data" => data = %{"type" => "variables", "attributes" => _variable_params}}) do
     changeset = Variable.changeset(%Variable{}, Params.to_attributes(data))
-    require IEx
-    IEx.pry
-
     case Repo.insert_with_cache(changeset) do
       {:ok, variable} ->
         conn
