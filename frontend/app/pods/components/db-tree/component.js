74c80da8f6091509ef90bd7a9df2eb531ed81cef
Aditya <im.adityau@gmail.com>
2018-05-10 12:05:32 +0530

    fix db-tree when question is already loasded

--- frontend/app/pods/components/db-tree/component.js
+++ frontend/app/pods/components/db-tree/component.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    tables: Ember.computed('database.tables', 'database.tables.isLoaded', 'query', function () {
-        let tables = this.get('database.tables');
+    db: Ember.computed('database', function () {
+        let database = this.get('database');
+        return this.get('store').peekRecord('database', database.id) || this.get('store').findRecord('database', database.id);
+    }),
+    tables: Ember.computed('db', 'db.isLoaded', 'db.tables', 'db.tables.isLoaded', 'query', function () {
+        let tables = this.get('db.tables');
         let query = this.get('query');
         query = query && query.trim();
         if (tables && query && query !== '') {
