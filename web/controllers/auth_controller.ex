cb2d496b9eb32a769a2a31668cc9a66575828b3b
Aditya U <im.adityau@gmail.com>
2019-05-01 23:59:56 +0530

    fix viewer permission

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -9,48 +9,69 @@ defmodule AfterGlow.AuthController do
   alias AfterGlow.CacheWrapper
   alias AfterGlow.CacheWrapper.Repo
 
-
   def google_auth_path(conn, _params) do
     conn
-    |> json %{path: authorize_url!("google")}
+    |> json(%{path: authorize_url!("google")})
   end
 
   def verify_token(conn, t) do
-    verified = t["token"]
-    |> Authorization.verify_token
+    verified =
+      t["token"]
+      |> Authorization.verify_token()
+
     if verified.error do
       conn
       |> put_status(:unauthorized)
-      |> json %{error: "invalid token"}
+      |> json(%{error: "invalid token"})
     else
-      perm = CacheWrapper.get_by_id(User, verified.claims["id"]) |> Repo.preload(permission_sets: :permissions) |> permissions
+      perm =
+        CacheWrapper.get_by_id(User, verified.claims["id"])
+        |> Repo.preload(permission_sets: :permissions)
+        |> permissions
+
       conn
-      |> json %{success: true, user: verified.claims, permissions: perm}
+      |> json(%{success: true, user: verified.claims, permissions: perm})
     end
   end
 
   def permissions(user) do
     user.permission_sets
-    |> Enum.map(fn x -> x.permissions |> Enum.map(fn y -> y.name end) end) |> List.flatten
+    |> Enum.map(fn x -> x.permissions |> Enum.map(fn y -> y.name end) end)
+    |> List.flatten()
   end
 
   def callback(conn, %{"provider" => provider, "code" => code}) do
     code = URI.decode(code)
     token = get_token!(provider, code)
     user = get_user!(provider, token)
+
     case user |> validate_email do
       true ->
         {:ok, user} = save_or_update_user(user)
         auth_token = create_jwt(user)
-        perm = user |> Repo.preload([permission_sets: :permissions]) |> permissions
+        perm = user |> Repo.preload(permission_sets: :permissions) |> permissions
+
         conn
         |> put_status(:created)
         |> put_resp_header("content-type", "application/json")
-        |> json %{token: auth_token, permissions: perm, user:  %{id: user.id, email: user.email, full_name: user.full_name, profile_pic: user.profile_pic}}
+        |> json(%{
+          token: auth_token,
+          permissions: perm,
+          user: %{
+            id: user.id,
+            email: user.email,
+            full_name: user.full_name,
+            profile_pic: user.profile_pic
+          }
+        })
+
       false ->
         conn
         |> put_status(:unprocessable_entity)
-        |> json %{error: "Only #{Application.get_env(:afterglow, :allowed_google_domain)} users are allowed "}
+        |> json(%{
+          error:
+            "Only #{Application.get_env(:afterglow, :allowed_google_domain)} users are allowed "
+        })
     end
   end
 
@@ -62,10 +83,9 @@ defmodule AfterGlow.AuthController do
     Google.authorize_url!(scope: "email")
   end
 
-
   defp create_jwt(user) do
     %{id: user.id, email: user.email, full_name: user.full_name, profile_pic: user.profile_pic}
-    |> Authorization.create_token
+    |> Authorization.create_token()
   end
 
   defp authorize_url!(_) do
@@ -81,21 +101,47 @@ defmodule AfterGlow.AuthController do
   end
 
   defp save_or_update_user(user) do
-    saved_user = Repo.one(from u in User, where: u.email == ^user["email"])
-     {:ok ,user} =if saved_user do
-      changeset = User.changeset(saved_user, %{email: user["email"], first_name: user["given_name"], last_name: user["family_name"], profile_pic: user["picture"], metadata: user, full_name: user[ "name" ]})
-       Repo.update_with_cache(changeset)
+    saved_user = Repo.one(from(u in User, where: u.email == ^user["email"]))
+
+    if saved_user do
+      changeset =
+        User.changeset(saved_user, %{
+          email: user["email"],
+          first_name: user["given_name"],
+          last_name: user["family_name"],
+          profile_pic: user["picture"],
+          metadata: user,
+          full_name: user["name"]
+        })
+
+      Repo.update_with_cache(changeset)
     else
-      changeset = User.changeset(%User{}, %{email: user["email"], first_name: user["given_name"], last_name: user["family_name"], profile_pic: user["picture"], metadata: user, full_name: user[ "name" ]})
+      changeset =
+        User.changeset(%User{}, %{
+          email: user["email"],
+          first_name: user["given_name"],
+          last_name: user["family_name"],
+          profile_pic: user["picture"],
+          metadata: user,
+          full_name: user["name"]
+        })
+
       {:ok, u} = Repo.insert_with_cache(changeset)
-      permission_set = Repo.one(from ps in PermissionSet, where: ps.name == "Viewer")
-      user_permission_set = Repo.insert_with_cache(UserPermissionSet.changeset(%UserPermissionSet{}, %{user_id: user.id, permission_set_id: permission_set.id}))
+      permission_set = Repo.one(from(ps in PermissionSet, where: ps.name == "Viewer"))
+
+      Repo.insert_with_cache(
+        UserPermissionSet.changeset(%UserPermissionSet{}, %{
+          user_id: u.id,
+          permission_set_id: permission_set.id
+        })
+      )
+
       {:ok, u}
     end
   end
 
   defp get_user!("google", token) do
-        user = OAuth2.Client.get!(token, "https://www.googleapis.com/plus/v1/people/me/openIdConnect")
-        user.body
+    user = OAuth2.Client.get!(token, "https://www.googleapis.com/plus/v1/people/me/openIdConnect")
+    user.body
   end
 end
